e019d8d4729a18b9d0e1f5e5a2b5ee4bd8c334ab
Helixo <103549104+helixoTR@users.noreply.github.com>
2023-01-02 20:14:23 +0000

    Updates

---
+++ precache-manifest.d5e4d1ccb81566c9805360c65cd956ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be65e8ef6398b908bc8211c2d33affd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3dc4309c3d537dc4e0a",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "107cc70532921c448fbe",
+    "url": "/static/css/main.98e576d8.chunk.css"
+  },
+  {
+    "revision": "d3dc4309c3d537dc4e0a",
+    "url": "/static/js/2.3490b23b.chunk.js"
+  },
+  {
+    "revision": "484b3a90b75ec38dbb4ca2cd8fcfc3fd",
+    "url": "/static/js/2.3490b23b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107cc70532921c448fbe",
+    "url": "/static/js/main.a8f3b52f.chunk.js"
+  },
+  {
+    "revision": "c044c296fef9e582399f",
+    "url": "/static/js/runtime-main.4070baf2.js"
+  },
+  {
+    "revision": "7c4aed56245ae966a682cb5ab46dee43",
+    "url": "/static/media/profile.7c4aed56.png"
+  }
+]);
